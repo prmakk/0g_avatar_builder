c8e6717458f3f0dc135b458076d4f671f5eb641f
unknown <priymakvlad420@gmail.com>
2025-03-23 20:42:00 +0200

    little refactor/redesign

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import * as fabric from "fabric";
 function App() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
+    const [isImageAdded, setIsImageAdded] = useState(false);
+    const [imageFile, setImageFile] = useState<File | null>(null);
 
     useEffect(() => {
-        if (canvasRef.current) {
+        if (canvasRef.current && isImageAdded) {
             const initCanvas = new fabric.Canvas(canvasRef.current, {
                 width: 500,
                 height: 500,
@@ -20,7 +22,24 @@ function App() {
                 initCanvas.dispose();
             };
         }
-    }, []);
+    }, [isImageAdded]);
+
+    useEffect(() => {
+        if (canvas && imageFile) {
+            const img = new Image();
+            img.src = URL.createObjectURL(imageFile);
+
+            img.onload = () => {
+                const fabricImage = new fabric.Image(img);
+
+                canvas.add();
+                canvas.add(fabricImage);
+                canvas.centerObject(fabricImage);
+                canvas.setActiveObject(fabricImage);
+                canvas.renderAll();
+            };
+        }
+    }, [canvas, imageFile]);
 
     const handleAddImage = (e) => {
         const file = e.target.files[0];
@@ -30,41 +49,8 @@ function App() {
             return;
         }
 
-        const reader = new FileReader();
-
-        reader.onload = (event) => {
-            const imageUrl = event.target?.result;
-
-            if (!imageUrl) {
-                console.error("Failed to read the file.");
-                return;
-            }
-
-            const imageElement = new Image();
-            imageElement.src = imageUrl;
-
-            imageElement.onload = () => {
-                const fabricImage = new fabric.Image(imageElement);
-
-                if (canvas) {
-                    canvas.add();
-                    canvas.add(fabricImage);
-                    canvas.centerObject(fabricImage);
-                    canvas.setActiveObject(fabricImage);
-                    canvas.renderAll();
-                }
-            };
-
-            imageElement.onerror = () => {
-                console.error("Failed to load the image.");
-            };
-        };
-
-        reader.onerror = () => {
-            console.error("Error reading the file.");
-        };
-
-        reader.readAsDataURL(file);
+        setImageFile(file);
+        setIsImageAdded(true);
     };
 
     const addGlasses = () => {
@@ -116,29 +102,55 @@ function App() {
     };
 
     return (
-        <main className="max-w-5xl mx-auto">
-            <header className="flex justify-center py-4">
-                <div className="flex flex-col gap-5 items-center justify-center">
-                    <img src="/logo.svg" alt="logo" className="max-w-40" />
-                    <h1 className="font-semibold text-4xl text-purple">
-                        0G avatar builder
-                    </h1>
-                </div>
-            </header>
+        <main className="min-h-screen bg-gradient-to-l from-[#FFA18F] via-[#B14EFF] to-[#3BADFF]">
+            <div className="max-w-5xl mx-auto pt-10">
+                <header className="flex justify-center py-4 rounded-2xl bg-white/30 backdrop-blur-sm">
+                    <div className="flex flex-col gap-5 items-center justify-center">
+                        <img src="/logo.svg" alt="logo" className="max-w-40" />
+                        <h1 className="font-semibold text-4xl text-white">
+                            OG AVATAR BUILDER
+                        </h1>
+                    </div>
+                </header>
 
-            <canvas ref={canvasRef} className="border-2" />
-            <input type="file" accept="image/*" onChange={handleAddImage} />
-            <button onClick={handleClearCanvas}>Clear</button>
-            <button onClick={addGlasses} className="cursor bg-red-400">
-                GLasses
-            </button>
-            <button
-                onClick={() =>
-                    saveCanvasAsImage(canvas!, "my_canvas_image.png")
-                }
-            >
-                Download👓
-            </button>
+                {!isImageAdded && (
+                    <div className="max-w-full flex justify-center items-center mt-20">
+                        <div>
+                            <input
+                                className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-100"
+                                aria-describedby="file_input_help"
+                                id="file_input"
+                                type="file"
+                                accept="image/*"
+                                onChange={handleAddImage}
+                            />
+                        </div>
+                    </div>
+                )}
+
+                {isImageAdded && (
+                    <div className="p-10 flex justify-center items-center">
+                        <canvas ref={canvasRef} className="border-2" />
+                        <button onClick={handleClearCanvas}>Clear</button>
+                        <button
+                            onClick={addGlasses}
+                            className="cursor bg-red-400"
+                        >
+                            Glasses
+                        </button>
+                        <button
+                            onClick={() =>
+                                saveCanvasAsImage(
+                                    canvas!,
+                                    "my_canvas_image.png"
+                                )
+                            }
+                        >
+                            Download👓
+                        </button>
+                    </div>
+                )}
+            </div>
         </main>
     );
 }
